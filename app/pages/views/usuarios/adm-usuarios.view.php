c769c4dcc43831da338a473e7b3205bbb4377d48
root <roberto.stzutski.com>
2022-03-07 00:56:51 -0300

    implementos modulo clientes

--- app/pages/views/usuarios/adm-usuarios.view.php
+++ app/pages/views/usuarios/adm-usuarios.view.php
@@ -1,1 +1,42 @@
-administração de usuários
+<div class="card">
+  <div class="card-header">
+    <b>Administrar Usuários</b>
+  </div>
+  <div class="card-body">
+
+
+
+  <form>
+  <div class="form-group">
+    <label for="useruid">Selecione um usuário para editar</label>
+    <select name="useruid" id="useruid" class="form-control">
+      <option value="">Selecione</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="nomeusuario">Nome do usuário</label>
+    <input type="text" class="form-control" id="nomeusuario" name="nomeusuario">
+  </div>
+  <div class="form-group">
+    <label for="nomeusuario">Nome do usuário</label>
+    <input type="text" class="form-control" id="nomeusuario" name="nomeusuario">
+  </div>
+  <div class="form-group">
+    <label for="nomeusuario">Nome do usuário</label>
+    <input type="text" class="form-control" id="nomeusuario" name="nomeusuario">
+  </div>
+  <div class="form-group">
+    <label for="nomeusuario">Nome do usuário</label>
+    <input type="text" class="form-control" id="nomeusuario" name="nomeusuario">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+
+  </div>
+  <div class="card-footer text-muted">
+    <small><b>UFO WAY LABS - TI TEAM</b></small>
+  </div>
+</div>
